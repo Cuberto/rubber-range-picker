1c76e0f3e6cc25326944d71add3ad30a96ed8563
Evgeniy <7850039+own2pwn@users.noreply.github.com>
2019-02-27 11:12:31 +0300

    iOS 8 framework project + target

---
+++ rubber-range-picker/Rubber Range Picker/Rubber Range Picker/Rubber_Range_Picker.h
@@ -0,0 +1,19 @@
+//
+//  Rubber_Range_Picker.h
+//  Rubber Range Picker
+//
+//  Created by Evgeniy on 27/02/2019.
+//  Copyright © 2019 Surge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rubber_Range_Picker.
+FOUNDATION_EXPORT double Rubber_Range_PickerVersionNumber;
+
+//! Project version string for Rubber_Range_Picker.
+FOUNDATION_EXPORT const unsigned char Rubber_Range_PickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rubber_Range_Picker/PublicHeader.h>
+
+
